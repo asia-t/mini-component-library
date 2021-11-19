a4a655439220720b8ef5a6448f0ea8b6eb200d99
Joanna Derks <joanna.derks@schibsted.pl>
2021-11-19 14:53:16 +0100

    progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,54 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZES = {
+  small: {
+    "--borderRadius": 4 + "px",
+    "--height": 8 + "px",
+  },
+  medium: {
+    "--borderRadius": 4 + "px",
+    "--height": 12 + "px",
+  },
+  large: {
+    display: 'flex',
+    "--borderRadius": 8 + "px",
+    "--height": 24 + "px",
+  }
 };
+
+const innerBarLargeStyles = {
+    "--height": 16 + "px",
+    "--borderRadius": 4 + "px",
+    "--margin": "4px"
+}
+
+const ProgressBar = ({ value, size }) => {
+  const styles = SIZES[size];
+  const barStyles = size === 'large' ? innerBarLargeStyles : null;
+
+  return <Wrapper role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100" style={styles}>
+    <Bar value={value} style={barStyles}/>
+    <VisuallyHidden>{value}</VisuallyHidden>
+  </Wrapper>
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: var(--height);
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--borderRadius);
+  overflow: hidden;
+`
+
+const Bar = styled.div`
+  width: ${props => props.value}%;
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  margin: var(--margin);
+  border-radius: var(--borderRadius) 0 0 var(--borderRadius);
+  
+`
 
 export default ProgressBar;
